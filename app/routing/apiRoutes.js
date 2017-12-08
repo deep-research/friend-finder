a2ee7b6a6947cbb45897bd0cea9c3232a4794d64
Victor <deep-research>
2017-12-08 17:46:25 -0600

    Program working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,17 +1,48 @@
 var path = require("path");
+var randomInt = require("random-int")
 
 var friendsData = require("../data/friends");
 
 module.exports = function(app) {
     app.get("/api/friends", function(req, res) {
-        // Displays a JSON of all possible friends
         res.json(friendsData)
     });
   
     app.post("/api/friends", function(req, res) {
         // Handle incoming survey results and compatibiliy logic
-        console.log(req)
         var newFriend = req.body
+        var newFriendScores = newFriend.scores
+
+        if (friendsData.length > 0) {
+            var friendComparisons = []
+
+            for (i=0; i<friendsData.length; i++) {
+                var potentialMatchScores = friendsData[i].scores
+                var totalDifference = 0
+                for (j=0; j<potentialMatchScores.length; j++) {
+                    var questionDifference = Math.abs(potentialMatchScores[j] - newFriendScores[j])
+                    totalDifference += questionDifference
+                }
+                friendComparisons.push(totalDifference)
+            }
+
+            var minimumValue = Math.min.apply(null, friendComparisons)
+            var bestMatches = []
+            for (i=0; i<friendComparisons.length; i++) {
+                if (friendComparisons[i] === minimumValue) {
+                    bestMatches.push(i)
+                }
+            }
+
+            var randomNumber = randomInt(bestMatches.length-1)
+            var bestMatchIndex = bestMatches[randomNumber]           
+            var chosenMatch = friendsData[bestMatchIndex]
+
+            res.json(chosenMatch)
+        } else {
+            res.json()
+        }
+
         friendsData.push(newFriend)
     });
 };
