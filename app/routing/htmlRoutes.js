871619d8f197cc4d72ac87fa82fce98347cc6b8b
Victor <deep-research>
2017-12-10 17:56:32 -0600

    htmlRoutes.js spacing

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,11 +3,11 @@ var path = require("path");
 module.exports = function(app) {
     // Display the survey page
     app.get("/survey", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/survey.html"));
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
   
     // Display the home page
     app.get("*", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/home.html"));
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 };
